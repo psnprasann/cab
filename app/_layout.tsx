5f4ab45db1119a52bc54c5e51995e30bb061d58b
Psn <106792309+psnprasann@users.noreply.github.com>
2025-11-19 13:51:58 +0530

    Create _layout.tsx

---
+++ app/_layout.tsx
@@ -0,0 +1,37 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { DriverProvider } from "@/contexts/DriverContext";
+
+SplashScreen.preventAutoHideAsync();
+
+const queryClient = new QueryClient();
+
+function RootLayoutNav() {
+  return (
+    <Stack screenOptions={{ headerBackTitle: "Back", headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="login" />
+      <Stack.Screen name="driver" />
+      <Stack.Screen name="admin" />
+    </Stack>
+  );
+}
+
+export default function RootLayout() {
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <DriverProvider>
+          <RootLayoutNav />
+        </DriverProvider>
+      </GestureHandlerRootView>
+    </QueryClientProvider>
+  );
+}
